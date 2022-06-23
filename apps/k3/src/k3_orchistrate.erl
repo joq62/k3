67cfaadde26572c825342a791d9337b393fedb9f
joq62 <joakim.leche@gmail.com>
2022-06-23 21:45:17 +0200

    corr in remote-host

--- apps/k3/src/k3_orchistrate.erl
+++ apps/k3/src/k3_orchistrate.erl
@@ -40,15 +40,29 @@
 desired_state(DeploymentName)->
     {ok,Hosts}=db_deployments:read(hosts,DeploymentName),
     AllK3Nodes=sd:get(k3),
+    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+				 {"DEBUG: AllK3Nodes   ",AllK3Nodes}]),
     AllK3Hosts=[rpc:call(Node,net,gethostname,[],5000)||Node<-AllK3Nodes],
+    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+				 {"DEBUG: AllK3Hosts   ",AllK3Hosts}]),
+
     MissingHosts=[HostName||HostName<-Hosts,
 			    false=:=lists:member({ok,HostName},AllK3Hosts)],
 
+    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+				 {"DEBUG: MissingHosts   ",MissingHosts}]),
+
     FailedK3HostsNodes=[rpc:call(Node,net,gethostname,[],5000)||Node<-AllK3Nodes,
 								      pong/=rpc:call(Node,k3,ping,[],5000)],
+    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+				 {"DEBUG: FailedK3HostsNodes   ",FailedK3HostsNodes}]),
+
     FailedK3Hosts=[HostName||{ok,HostName}<-FailedK3HostsNodes],
     
     HostsToRestart=lists:append([MissingHosts,FailedK3Hosts]),
+    rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
+				 {"DEBUG: HostsToRestart   ",HostsToRestart}]),
+
     Reply=case HostsToRestart of
 	      []->
 		  rpc:cast(node(),nodelog,log,[notice,?MODULE_STRING,?LINE,
